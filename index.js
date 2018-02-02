c30a66961255ec41aa12f3ff1fe99654ec45476a
Van Pho Nguyen <vanpho02@gmail.com>
2018-02-02 18:08:59 +0700

    constructor

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const data = require('./data');
 
+class Tin {
+    constructor(id, title, description, image, link) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.link = link;
+    }
+}
